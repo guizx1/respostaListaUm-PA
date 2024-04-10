a313fc067cc573584e2c6f34ead6ec0a8b4d2aa6
Guilherme Abraão <166646459+guizx1@users.noreply.github.com>
2024-04-10 17:32:08 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,34 @@
+
+public class Cachorro {
+	private String raca;
+	private String nome;
+	private int idade;
+	
+	public Cachorro(String raca, String nome, int idade) {
+		this.raca = raca;
+		this.nome = nome;
+		this.idade = idade;
+	}
+	public String getRaca() {
+		return raca;
+	}
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String latir () {
+		return "Au au!"
+	}
+
+}
