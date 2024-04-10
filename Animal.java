4c8bb15e2eebdae96deefa69310be2b16699ba28
Guilherme Abraão <166646459+guizx1@users.noreply.github.com>
2024-04-10 17:47:27 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,31 @@
+
+public class Animal {
+	private String nome;
+	private String tipo;
+	private boolean emitir;
+	
+	
+	public Animal (String nome, String tipo,boolean emitir) {
+		this.nome = nome;
+		this.tipo = tipo;
+		this.emitir = emetir;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public boolean emitirSom() {
+		this.emitir = true
+	}
+}
